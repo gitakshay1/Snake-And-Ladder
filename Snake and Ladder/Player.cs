29765cf6dd5d01dff8c827cfcf0f5e6964d7ad33
gitakshay1 <akshaypatil309p8@gmail.com>
2023-01-04 15:59:13 +0530

    [Akshay] Added program for Two players

--- Snake and Ladder/Player.cs
+++ Snake and Ladder/Player.cs
@@ -8,37 +8,68 @@ namespace Snake_and_Ladder
 {
     public class Player
     {
-        int Position1 = 0, Player1,Num_Dice_Roll=0;
+        int Position1=0 ,Position2=0 ,Position , Dice,Num_Dice_Roll_Plyr1=0, Num_Dice_Roll_Plyr2=0, Turn=0,player=0,
+            Num_Dice_Roll;
         public void RollDice()
         {
             Random random= new Random();
-            while(Position1<100)
+            while(Position<100)
             {
-                Player1 = random.Next(1, 7);
+                Dice = random.Next(1, 7);
                 int check1 = random.Next(0, 3);
+                if(Turn==0)
+                {
+                    Position = Position1;
+                    player = 1;
+                }
+                else
+                {
+                    Position = Position2;
+                    player = 2;
+                }
                 switch (check1)
                 {
                     case 0:
                         break;
                     case 1:
-                        Position1 += Player1;
-                        if(Position1>100)
+                        Position += Dice;
+                        if(Position>100)
                         {
-                            Position1 -= Player1;
+                            Position -= Dice;
                         }
                         break;
                     case 2:
-                        Position1 -= Player1;
-                        if (Position1 < 0)
+                        Position -= Dice;
+                        if (Position < 0)
                         {
-                            Position1 =0;
+                            Position =0;
                         }
                         break;
                 }
-                Num_Dice_Roll++;
-                Console.WriteLine("Player1 postion at" + Position1);
+                if(player==1)
+                {
+                    Position1= Position;
+                    Num_Dice_Roll_Plyr1++;
+                    if (check1 != 1)
+                    {
+                        Turn = 1;
+                    }
+                    Num_Dice_Roll = Num_Dice_Roll_Plyr1;
+                }
+                else
+                {
+                    Position2 = Position;
+                    Num_Dice_Roll_Plyr2++;
+                    if (check1!= 1)
+                    {
+                        Turn = 0;
+                    }
+                    Num_Dice_Roll= Num_Dice_Roll_Plyr2;
+                }
+                
+                Console.WriteLine("Player1 at " + Position1+" And Player2 at "+Position2);
             }
-            Console.WriteLine("\nPlayer1 won At Position" + Position1+"\nNumber of Dice rolls to win"+Num_Dice_Roll);
+            Console.WriteLine("\nPlayer"+player+" won At Position" + Position+"\nNumber of Dice rolls to win"+Num_Dice_Roll);
         }
     }
 }
